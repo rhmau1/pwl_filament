31bdbefda519c41fd038d95ad58daa6fbb2926db
Fijri <fijriatirahmaturrizqi@gmail.com>
2026-04-07 15:44:27 +0700

    feat: create PostForm schema with detailed input fields and layout configuration and validation

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -8,33 +8,63 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\RichEditor;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\TagsInput;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Schema;
 
 class PostForm
 {
     public static function configure(Schema $schema): Schema
     {
-        return $schema->components([
-            //
-            TextInput::make("title")->required()
-                ->minLength(5),
-            TextInput::make("slug")->required()->unique(),
-            Select::make("category_id")
-                ->label("Category")
-                ->options(\App\Models\Category::all()->pluck("name", "id"))
-                ->required(),
-            ColorPicker::make("color"),
-            MarkdownEditor::make("body"),
-            RichEditor::make("body"),
-            FileUpload::make("image")
-                ->disk("public")
-                ->directory("post"),
-            TagsInput::make("tags"),
-            Checkbox::make('published'),
-            DatePicker::make('published_at')
-        ]);
+        return $schema
+            ->components([
+                Group::make([
+                    Section::make("Post Detail")
+                        ->description("Enter the post details below.")
+                        ->icon("heroicon-o-document-text")
+                        ->schema([
+                            TextInput::make("title")
+                                // ->rules('required'| 'min:5'| 'max:255'),
+                                ->required()
+                                ->minLength(5)
+                                ->maxLength(255),                            
+                        TextInput::make("slug")->required()
+                        ->minLength(3)
+                        ->unique()
+                        ->validationMessages([
+                            'unique' => 'Slug harus unik dan tidak boleh sama.'
+                        ]),
+                            Select::make("category_id")
+                                ->required()
+                                ->relationship("category", "name")
+                                ->preload()
+                                ->searchable()
+                                ->validationMessages([
+                                    'required' => 'category harus dipilih'
+                                ]),
+                            ColorPicker::make("color"),
+                        ])->columns(2),
+                ])->columnSpan(2),
+                Group::make([
+                    Section::make("Image Upload")
+                        ->icon("heroicon-o-photo")
+                        ->schema([
+                            FileUpload::make("image")
+                                ->required()
+                                ->disk("public")
+                                ->directory("post"),
+                        ]),
+                    Section::make("Meta")
+                        ->icon("heroicon-o-tag")
+                        ->schema([
+                            TagsInput::make("tags"),
+                            Checkbox::make('published'),
+                            DatePicker::make('published_at'),
+                        ]),
+                ])->columnSpan(1),
+            ])->columns(3);
     }
 }
